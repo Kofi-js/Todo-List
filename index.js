15435f9332c2ea54f098df2243685d2f307c776f
Kofi <vwegbakofi@gmail.com>
2022-07-07 15:15:37 +0100

    New commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./db");
 const { json } = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const todo = require("./controllers/todoController");
 const models = require("./models/todoModels");
 const routes = require("./routes/todoRoute");
@@ -11,7 +13,8 @@ connectDB();
 const app = express();
 
 app.use(json());
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT;
 
 app.use("/", routes);
